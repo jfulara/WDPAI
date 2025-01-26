62837e0d7f547bbafd432df9e54686b66430d4bc
Jakub Fulara <fularajakub@onet.pl>
2025-01-26 02:43:10 +0100

    Routing added

--- Routing.php
+++ Routing.php
@@ -4,13 +4,18 @@ require_once 'src/controllers/DashboardController.php';
 require_once 'src/controllers/SecurityController.php';
 
 class Routing{
+    public static $routes;
+
+    public static function get($url, $controller){
+        self::$routes[$url] = $controller;
+    }
     public static function run ($url) {
         $action = explode("/", $url)[0];
         $controller = null;
 
-        // if (!array_key_exists($action, self::$routes)) {
-        //   die("Wrong url!"); //TODO 404
-        // }
+        if (!array_key_exists($action, self::$routes)) {
+            die("Wrong url!"); //TODO 404
+        }
 
         
         if(in_array($action, ["dashboard", ""])){
@@ -25,7 +30,7 @@ class Routing{
     
         //$controller = self::$routes[$action];
         $object = new $controller;
-        $action = $action ?: 'index';
+        //$action = $action ?: 'index';
     
         $object->$action();
     }
--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once 'src/controllers/AppController.php';
 
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
-$path = parse_url( $path, PHP_URL_PATH);
+$path = parse_url($path, PHP_URL_PATH);
 
+Routing::get('login', 'SecurityController');
+Routing::get('dashboard', 'DashboardController');
 Routing::run($path);
